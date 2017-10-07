0854c3d26ab3786db099cceaf792861115dc37f2
Richard Tiutiun <rtiutiun@e1z3r10p3.42.us.org>
2017-10-07 01:38:38 -0700

    wtf

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -16,13 +16,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	if (!n)
 		return (0);
-	while ((int)n > 0 && (*s1 || *s2))
+	while ((int)n-- > 0 && (*s1 || *s2))
 	{
 		if (*s1 != *s2)
 			return (*(unsigned char *)s1 - *(unsigned char*)s2);
 		s1++;
 		s2++;
-		n--;
 	}
 	return (0);
 }
